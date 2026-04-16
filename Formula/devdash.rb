8761370760d33096e3608d4481cf09a96f3d7240
Nicolas Claisse <nicolas@claisse-prog.fr>
2026-04-16 15:12:15 +0200

    💸 ajouter la formule devdash v0.1.0

---
+++ Formula/devdash.rb
@@ -0,0 +1,23 @@
+class Devdash < Formula
+  desc "Web dashboard to launch and monitor a process-compose/nix dev environment"
+  homepage "https://github.com/nicolasclaisse/devdash"
+  url "https://github.com/nicolasclaisse/devdash/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "4b90e903151de7a367def8a5a0b6fcb3d2731e055bf93b14d08f0bae2f5eedd1"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install"
+    system "npm", "run", "build"
+    system "npm", "prune", "--omit=dev"
+
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/devdash.js" => "devdash"
+  end
+
+  test do
+    assert_predicate libexec/"dist/server.js", :exist?
+    assert_predicate libexec/"bin/devdash.js", :exist?
+  end
+end
