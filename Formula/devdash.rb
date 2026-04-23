73dc3bab575c341699e98d1c086da6d7769ffbd8
Nicolas Claisse <nicolas@claisse-prog.fr>
2026-04-23 15:17:12 +0200

    devdash v0.6.1

--- Formula/devdash.rb
+++ Formula/devdash.rb
@@ -1,8 +1,8 @@
 class Devdash < Formula
   desc "Web dashboard to launch and monitor a process-compose/nix dev environment"
   homepage "https://github.com/nicolasclaisse/devdash"
-  url "https://github.com/nicolasclaisse/devdash/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "7adf81bdf44277b31962a4bc218e944be857ae44514f5b59522ead16979b6467"
+  url "https://github.com/nicolasclaisse/devdash/archive/refs/tags/v0.6.1.tar.gz"
+  sha256 "536f8eec91366e85f36726c0ec1f554476c1757fa46e8f1a159fb3f26b5fe70c"
   license "MIT"
 
   depends_on "node"
